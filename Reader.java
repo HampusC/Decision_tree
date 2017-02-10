1e0ffd09ec88c89af8d79fc13f09561169c84aa3
Tim Borglund <dic13tbo@mars-11.student.lth.se>
2017-02-10 16:59:30 +0100

    ;)

--- Reader.java
+++ Reader.java
@@ -1,6 +1,25 @@
+import weka.core.Instances;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
-import weka.core.Attribute;
 public class Reader {
-	
+
+	public Reader(){
+
+	}
+
+	public void read(){
+		BufferedReader reader = null;
+		Instances data = null;
+		try{
+			  reader = new BufferedReader(new FileReader("./data.arff"));
+			  data = new Instances(reader);
+			//  reader.close();
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+
+		// data.setClassIndex(data.numAttributes() - 1);
+	}
 
 }
